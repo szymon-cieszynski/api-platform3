64fda0020bb7cf117e0251fbc483fed643920dca
szymon cieszynski <cieszynski8@gmail.com>
2023-02-22 12:10:38 +0100

    profiler and new fields

---
+++ src/Entity/DragonTreasure.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\DragonTreasureRepository;
+use Carbon\Carbon;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\Locale;
+
+#[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
+#[ApiResource(
+    shortName: 'Treasure',
+    description: 'A rare and valuable treasure.',
+    operations: [
+        new Get(),
+        new getCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+
+    ]
+)]
+class DragonTreasure
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    /**
+     * The estimated value of this treasure, in gold coins.
+     */
+    #[ORM\Column]
+    private ?int $value = null;
+
+    #[ORM\Column]
+    private ?int $coolFactor = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $plunderAt;
+
+    #[ORM\Column]
+    private ?bool $isPublished = null;
+
+    public function __construct()
+    {
+        $this->plunderedAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+//    public function setDescription(string $description): self
+//    {
+//        $this->description = $description;
+//
+//        return $this;
+//    }
+
+    public function setTextDescription(string $description): self
+    {
+        $this->description = nl2br($description);
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCoolFactor(): ?int
+    {
+        return $this->coolFactor;
+    }
+
+    public function setCoolFactor(int $coolFactor): self
+    {
+        $this->coolFactor = $coolFactor;
+
+        return $this;
+    }
+
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getPlunderAt(): ?\DateTimeImmutable
+    {
+        return $this->plunderAt;
+    }
+
+    /**
+     * A human-readable representation of when this treasure was plundered.
+     */
+
+//    public function getPlunderAtAgo(): string
+//    {
+//        return Carbon::instance($this->plunderAt)->diffForHumans();
+//    }
+
+
+}
