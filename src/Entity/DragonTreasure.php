5ad79bc25ddf1b514f0fe06486d86025eee1d578
szymon cieszynski <cieszynski8@gmail.com>
2023-02-22 17:17:39 +0100

    data factory and fixtures for API

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -13,6 +13,8 @@ use App\Repository\DragonTreasureRepository;
 use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Validator\Constraints\Locale;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
@@ -26,7 +28,14 @@ use Symfony\Component\Validator\Constraints\Locale;
         new Put(),
         new Patch(),
 
-    ]
+    ],
+    normalizationContext: [
+        'groups' => ['treasure:read'],
+    ],
+    denormalizationContext: [
+        'groups' => ['treasure:write'],
+    ],
+    paginationItemsPerPage: 10,
 )]
 class DragonTreasure
 {
@@ -36,28 +45,33 @@ class DragonTreasure
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['treasure:read'])]
     private ?string $description = null;
 
     /**
      * The estimated value of this treasure, in gold coins.
      */
     #[ORM\Column]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?int $value = null;
 
     #[ORM\Column]
+    #[Groups(['treasure:read', 'treasure:write'])]
     private ?int $coolFactor = null;
 
     #[ORM\Column]
-    private ?\DateTimeImmutable $plunderAt;
+    private ?\DateTimeImmutable $plunderedAt;
 
     #[ORM\Column]
-    private ?bool $isPublished = null;
+    private bool $isPublished = false;
 
-    public function __construct()
+    public function __construct(string $name = null)
     {
+        $this->name = $name;
         $this->plunderedAt = new \DateTimeImmutable();
     }
 
@@ -71,25 +85,27 @@ class DragonTreasure
         return $this->name;
     }
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
+//    public function setName(string $name): self
+//    {
+//        $this->name = $name;
+//
+//        return $this;
+//    }
 
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
-//    public function setDescription(string $description): self
-//    {
-//        $this->description = $description;
-//
-//        return $this;
-//    }
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
 
+        return $this;
+    }
+
+    #[Groups(['treasure:write'])]
+    #[SerializedName('description')]
     public function setTextDescription(string $description): self
     {
         $this->description = nl2br($description);
@@ -136,18 +152,24 @@ class DragonTreasure
     /**
      * @return \DateTimeImmutable|null
      */
-    public function getPlunderAt(): ?\DateTimeImmutable
+    public function getPlunderedAt(): ?\DateTimeImmutable
     {
-        return $this->plunderAt;
+        return $this->plunderedAt;
+    }
+
+    public function setPlunderedAt(\DateTimeImmutable $plunderedAt): self
+    {
+        $this->plunderedAt = $plunderedAt;
+        return $this;
     }
 
     /**
      * A human-readable representation of when this treasure was plundered.
      */
-
-//    public function getPlunderAtAgo(): string
+    //#[Groups(['treasure:read'])]
+//    public function getPlunderedAtAgo(): string
 //    {
-//        return Carbon::instance($this->plunderAt)->diffForHumans();
+//        return Carbon::instance($this->plunderedAt)->diffForHumans();
 //    }
 
 
